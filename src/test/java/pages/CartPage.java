43de1d3315c33c30434cd16c91b5f562e9b62775
MarinaLozhkina <lozhkina.marina86@gmail.com>
2025-05-28 07:18:00 +0400

    Last changes after review

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CartPage extends BasePage {
+    private final By productsNamesLocator = By.cssSelector(".inventory_item_name");
+
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
     public ArrayList<String> getProductsNames() {
-        List<WebElement> allProductsNames = driver.findElements(By.cssSelector(".inventory_item_name"));
+        List<WebElement> allProductsNames = driver.findElements(productsNamesLocator);
         ArrayList<String> names = new ArrayList<>();
         for (WebElement product : allProductsNames) {
             names.add(product.getText());
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -27,7 +27,7 @@ public class BaseTest {
 
     @Parameters({"browser"})
     @BeforeMethod
-    @Step()
+    @Step("Инициализация тестового окружения")
     public void setup(@Optional("chrome") String browser, ITestContext context) {
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
@@ -51,7 +51,7 @@ public class BaseTest {
         password = PropertyReader.getProperty("saucedemo.password");
     }
 
-    @Step("Закрытие")
+    @Step("Закрытие браузера")
     @AfterMethod
     public void close() {
         driver.quit();
